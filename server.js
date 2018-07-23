a6191deeb53f7be554e3dd025d4778c5c7ed59ba
Valentin B <valentin.bacon56@gmail.com>
2018-07-23 20:48:52 +0200

    First Commit
    First commit, register ok

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connect to database
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected',()=>{
+    console.log('connected to database'+config.database)
+})
+mongoose.connection.on('error',(err)=>{
+    console.log('database error' + err)
+})
+
+
+const app = express();
+
+//Body Parser Middleware (to get data from post req)
+app.use(bodyParser.json());
+
+const users = require('./routes/users');
+
+//Port Number
+const port = 3000;
+
+//Autoriser les requetes cross-domain vers l'API
+app.use(cors());
+
+app.use('/users',users)
+
+
+//sTATIC fOLDER
+app.use(express.static(path.join(__dirname,'public')));
+
+//Index Route
+app.get('*', (req,res)=>{
+    res.send('Invalid Endpoint')
+})
+
+
+//Start Server
+app.listen(port, ()=>{
+    console.log('Server Running on port' + port)
+})
